f705c25f1bdb2b5372bbd6f82d3257cf2663dc0e
mathrabbit1028 <87797171+mathrabbit1028@users.noreply.github.com>
2023-06-09 12:16:23 +0900

    version 1.0 (IOI)

--- script.js
+++ script.js
@@ -1,106 +1,67 @@
-const taskInput = document.querySelector(".task-input input"),
-filters = document.querySelectorAll(".filters span"),
-clearAll = document.querySelector(".clear-btn"),
-taskBox = document.querySelector(".task-box");
+problems = document.querySelectorAll(".problem > a");
 
-let editId,
-isEditTask = false,
-todos = JSON.parse(localStorage.getItem("todo-list"));
+var current = [];
+current = JSON.parse(localStorage.getItem("current"));
 
-filters.forEach(btn => {
-    btn.addEventListener("click", () => {
-        document.querySelector("span.active").classList.remove("active");
-        btn.classList.add("active");
-        showTodo(btn.id);
-    });
-});
+function changingStatus(event) { 
+    const nowStatus = event.currentTarget.getAttribute("id");
+    const problemNumber = parseInt(event.currentTarget.firstChild.getAttribute("href").substr(32));
 
-function showTodo(filter) {
-    let liTag = "";
-    if(todos) {
-        todos.forEach((todo, id) => {
-            let completed = todo.status == "completed" ? "checked" : "";
-            if(filter == todo.status || filter == "all") {
-                liTag += `<li class="task">
-                            <label for="${id}">
-                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
-                                <p class="${completed}">${todo.name}</p>
-                            </label>
-                            <div class="settings">
-                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
-                                <ul class="task-menu">
-                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
-                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
-                                </ul>
-                            </div>
-                        </li>`;
-            }
-        });
+    if (nowStatus === "none") {
+        event.currentTarget.setAttribute("id", "trying");
+        current[problemNumber] = 1;
     }
-    taskBox.innerHTML = liTag || `<span>You don't have any task here</span>`;
-    let checkTask = taskBox.querySelectorAll(".task");
-    !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
-    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
-}
-showTodo("all");
-
-function showMenu(selectedTask) {
-    let menuDiv = selectedTask.parentElement.lastElementChild;
-    menuDiv.classList.add("show");
-    document.addEventListener("click", e => {
-        if(e.target.tagName != "I" || e.target != selectedTask) {
-            menuDiv.classList.remove("show");
-        }
-    });
-}
-
-function updateStatus(selectedTask) {
-    let taskName = selectedTask.parentElement.lastElementChild;
-    if(selectedTask.checked) {
-        taskName.classList.add("checked");
-        todos[selectedTask.id].status = "completed";
-    } else {
-        taskName.classList.remove("checked");
-        todos[selectedTask.id].status = "pending";
+    else if (nowStatus === "trying") {
+        event.currentTarget.setAttribute("id", "implementing");
+        current[problemNumber] = 2;
     }
-    localStorage.setItem("todo-list", JSON.stringify(todos))
-}
-
-function editTask(taskId, textName) {
-    editId = taskId;
-    isEditTask = true;
-    taskInput.value = textName;
-    taskInput.focus();
-    taskInput.classList.add("active");
-}
-
-function deleteTask(deleteId, filter) {
-    isEditTask = false;
-    todos.splice(deleteId, 1);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo(filter);
-}
-
-clearAll.addEventListener("click", () => {
-    isEditTask = false;
-    todos.splice(0, todos.length);
-    localStorage.setItem("todo-list", JSON.stringify(todos));
-    showTodo()
-});
-
-taskInput.addEventListener("keyup", e => {
-    let userTask = taskInput.value.trim();
-    if(e.key == "Enter" && userTask) {
-        if(!isEditTask) {
-            todos = !todos ? [] : todos;
-            let taskInfo = {name: userTask, status: "pending"};
-            todos.push(taskInfo);
-        } else {
-            isEditTask = false;
-            todos[editId].name = userTask;
-        }
-        taskInput.value = "";
-        localStorage.setItem("todo-list", JSON.stringify(todos));
-        showTodo(document.querySelector("span.active").id);
+    else if (nowStatus === "implementing") {
+        event.currentTarget.setAttribute("id", "solved");
+        current[problemNumber] = 3;
     }
-});
+    else if (nowStatus === "solved") {
+        event.currentTarget.setAttribute("id", "none");
+        current[problemNumber] = 0;
+    }
+    localStorage.setItem("current", JSON.stringify(current));
+}
+
+function rollback(event) {
+    const nowStatus = event.currentTarget.parentNode.getAttribute("id");
+    const problemNumber = parseInt(event.currentTarget.getAttribute("href").substr(32));
+
+    if (nowStatus === "none") {
+        event.currentTarget.parentNode.setAttribute("id", "solved");
+        current[problemNumber] = 3;
+    }
+    else if (nowStatus === "trying") {
+        event.currentTarget.parentNode.setAttribute("id", "none");
+        current[problemNumber] = 0;
+    }
+    else if (nowStatus === "implementing") {
+        event.currentTarget.parentNode.setAttribute("id", "trying");
+        current[problemNumber] = 1;
+    }
+    else if (nowStatus === "solved") {
+        event.currentTarget.parentNode.setAttribute("id", "implementing");
+        current[problemNumber] = 2;
+    }
+    localStorage.setItem("current", JSON.stringify(current));
+}
+
+for (var i = 0; i < problems.length; i++) {
+    problems[i].parentNode.addEventListener("click", changingStatus);
+    problems[i].addEventListener("click", rollback);
+    if (current[problems[i].getAttribute("href").substr(32)] === 0) {
+        problems[i].parentNode.setAttribute("id", "none");
+    }
+    else if (current[problems[i].getAttribute("href").substr(32)] === 1) {
+        problems[i].parentNode.setAttribute("id", "trying");
+    }
+    else if (current[problems[i].getAttribute("href").substr(32)] === 2) {
+        problems[i].parentNode.setAttribute("id", "implementing");
+    }
+    else if (current[problems[i].getAttribute("href").substr(32)] === 3) {
+        problems[i].parentNode.setAttribute("id", "solved");
+    }
+}
