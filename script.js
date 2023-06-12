91b7fbc4e9f8e6e2d6bb50b37f46c7af8f3a5d2c
mathrabbit1028 <87797171+mathrabbit1028@users.noreply.github.com>
2023-06-12 12:15:44 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
-document.cookie = "username=Max Brown";
+problems = document.querySelectorAll(".problem > a");
+
+var current = [];
+for (var i = 0; i < 100000; i++) current.push('');
+
+function changingStatus(event) { 
+    const nowStatus = event.currentTarget.getAttribute("id");
+    const problemNumber = parseInt(event.currentTarget.firstChild.getAttribute("href").substr(32));
+
+    if (nowStatus === "none") {
+        event.currentTarget.setAttribute("id", "trying");
+        current[problemNumber] = "1";
+    }
+    else if (nowStatus === "trying") {
+        event.currentTarget.setAttribute("id", "implementing");
+        current[problemNumber] = "2";
+    }
+    else if (nowStatus === "implementing") {
+        event.currentTarget.setAttribute("id", "solved");
+        current[problemNumber] = "3";
+    }
+    else if (nowStatus === "solved") {
+        event.currentTarget.setAttribute("id", "none");
+        current[problemNumber] = "";
+    }
+    var newCookie = "";
+    for (var i = 0; i < current.length; i++) {
+        if (current[i] != "") {
+            newCookie += i.toString() + "=" + current[i] + ";";
+        }
+    }
+    console.log(newCookie);
+    document.cookie = newCookie;
+}
+
+function rollback(event) {
+    const nowStatus = event.currentTarget.parentNode.getAttribute("id");
+    const problemNumber = parseInt(event.currentTarget.getAttribute("href").substr(32));
+
+    if (nowStatus === "none") {
+        event.currentTarget.parentNode.setAttribute("id", "solved");
+        current[problemNumber] = "3";
+    }
+    else if (nowStatus === "trying") {
+        event.currentTarget.parentNode.setAttribute("id", "none");
+        current[problemNumber] = "";
+    }
+    else if (nowStatus === "implementing") {
+        event.currentTarget.parentNode.setAttribute("id", "trying");
+        current[problemNumber] = "1";
+    }
+    else if (nowStatus === "solved") {
+        event.currentTarget.parentNode.setAttribute("id", "implementing");
+        current[problemNumber] = "2";
+    }
+    var newCookie = "";
+    for (var i = 0; i < current.length; i++) {
+        if (current[i] != "") {
+            newCookie += i.toString() + "=" + current[i] + ";";
+        }
+    }
+    console.log(newCookie);
+    document.cookie = newCookie;
+}
+
+for (var i = 0; i < problems.length; i++) {
+    problems[i].parentNode.addEventListener("click", changingStatus);
+    problems[i].addEventListener("click", rollback);
+    if (current[problems[i].getAttribute("href").substr(32)] === "") {
+        problems[i].parentNode.setAttribute("id", "none");
+    }
+    else if (current[problems[i].getAttribute("href").substr(32)] === "1") {
+        problems[i].parentNode.setAttribute("id", "trying");
+    }
+    else if (current[problems[i].getAttribute("href").substr(32)] === "2") {
+        problems[i].parentNode.setAttribute("id", "implementing");
+    }
+    else if (current[problems[i].getAttribute("href").substr(32)] === "3") {
+        problems[i].parentNode.setAttribute("id", "solved");
+    }
+}
